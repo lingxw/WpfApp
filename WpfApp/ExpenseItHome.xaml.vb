2fefba04acfefaaedacc5c1d0f2ae986befccd28
lingxw <idsun2010@qq.com>
2021-01-12 14:01:44 +0800

    add Page

---
+++ WpfApp/ExpenseItHome.xaml.vb
@@ -0,0 +1,8 @@
+﻿Class ExpenseItHome
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        ' View Expense Report
+        Dim expenseReportPage As New ExpenseReportPage()
+        Me.NavigationService.Navigate(expenseReportPage)
+    End Sub
+End Class
